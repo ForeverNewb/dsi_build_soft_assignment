8a0e41cd7fc98888666ed920978e813480546304
ForeverNewb <junasrustia@gmail.com>
2024-02-19 22:09:08 -0500

    Uploading local files

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='analysis',
+    version='0.1.0',
+    author='Analysis Team',
+    packages=['analysis'],
+    install_requires=[
+        'matplotlib',
+        'PyYAML',
+        'requests',
+        'json',
+        'pytest',
+        'logging'
+    ]
+)
